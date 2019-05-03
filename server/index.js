907f0c468194bb8e88a5166f3fda1a8cf8cf20d2
magh <manuel.gatica.holtmann@gmail.com>
2019-05-02 20:47:46 -0400

    definicion de puerto

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express=require('express');
 const app=express();
-app.listen(3000, ()=>{
+
+//Settings
+app.set('port',process.env.PORT || 3000);
+//Middlewares
+//Routes
+
+//Arranque de servidor
+app.listen(app.get('port'), ()=>{
     console.log('Servidor en puerto 3000');
 });
