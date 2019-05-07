b09bfd1d12d8cff74eaee3066e833396a5fc6b2c
magh <manuel.gatica.holtmann@gmail.com>
2019-05-06 23:21:29 -0400

    editar por Id

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -7,7 +7,6 @@ employeeController.getEmployees = async (req, res) => {
 
 employeeController.createEmployee= async(req, res)=>{
    const employee= new Employee(req.body);
-   console.log(employee);
    employee.save();
    res.json({"status":"OK"});
 };
@@ -15,7 +14,17 @@ employeeController.getEmployee=async (req,res)=>{
     const employee= await Employee.findById(req.params.id);
     res.json(employee);
 };
-employeeController.editEmployee=()=>{};
+employeeController.editEmployee= async (req,res)=>{
+    const {id}=req.params.id;
+    const employee={
+        name:req.params.name,
+        position:req.params.position,
+        salary:req.params.salary,
+        office:req.params.office
+    };
+    await Employee.findByIdAndUpdate(id, {$set:employee},{new:true});
+    res.json({"status":"edit ok"});
+};
 employeeController.deleteEmployee=()=>{};
 
 module.exports = employeeController;
