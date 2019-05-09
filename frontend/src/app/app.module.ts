fb579572fb79a50b05d7cb31098d38df2644ceb0
mgaticah <manuel.gatica@datco.cl>
2019-05-09 16:02:30 -0400

    enlace clase, componente, servicio employee

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { EmployeesComponent } from './components/employees/employees.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/models/employee.ts
+++ frontend/src/app/models/employee.ts
@@ -4,4 +4,12 @@ export class Employee {
     office:string;
     position:string;
     salary:number;
+    constructor(_id='', name='', office='', position='', salary=0)
+    {
+        this._id=_id;
+        this.name=name;
+        this.office=office;
+        this.position=position;
+        this.salary=salary;
+    }
 }
