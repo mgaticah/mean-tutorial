5af5c74d4c28e84f44cdc9519f599ef5bac5f926
mgaticah <manuel.gatica@datco.cl>
2019-05-09 17:18:32 -0400

    CORS configurado. método de grabación funcionando

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
-const express=require('express');
-const app=express();
-const morgan=require('morgan');
-const {mongoose}=require('./database');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { mongoose } = require('./database');
+const cors = require('cors');
 //Settings
-app.set('port',process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(cors({ origin: 'http://localhost:4200' }));
 //Routes
-app.use('/api/employees',require('./routes/employee.routes'));
+app.use('/api/employees', require('./routes/employee.routes'));
 //Arranque de servidor
-app.listen(app.get('port'), ()=>{
-    console.log('Servidor en puerto '+app.get('port'));
+app.listen(app.get('port'), () => {
+    console.log('Servidor en puerto ' + app.get('port'));
 });
