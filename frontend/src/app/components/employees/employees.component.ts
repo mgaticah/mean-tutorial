3e9d32b950eba736cac96490182899b2780eeb34
mgaticah <manuel.gatica@datco.cl>
2019-05-09 16:56:07 -0400

    implementación de limpieza de formulario

--- frontend/src/app/components/employees/employees.component.ts
+++ frontend/src/app/components/employees/employees.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
+import { NgForm } from '@angular/forms';
+import { Employee } from 'src/app/models/employee';
+
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css'],
-  providers:[EmployeeService]
+  providers: [EmployeeService]
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor(private employeeService:EmployeeService) {
-    
-   }
+  constructor(private employeeService: EmployeeService) {
+
+  }
 
   ngOnInit() {
   }
-
+  resetForm(form?: NgForm) {
+    if(form)
+    {
+      form.reset;
+    }
+    this.employeeService.selectedEmployee=new Employee();
+  }
 }
