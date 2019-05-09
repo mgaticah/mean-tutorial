c722a27ce38d1fb217d118d55172e8afb42d3e6d
mgaticah <manuel.gatica@datco.cl>
2019-05-09 15:38:23 -0400

    Definición de consultas http desde EmployeeService a Backend en localhost:3000/api/employees

--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Employee } from '../models/employee';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
-  selectedEmployee:Employee;
-  constructor(private http:HttpClient) {
-
-   }
+  readonly employeesApi = 'http://localhost:3000/api/employees';
+  selectedEmployee: Employee;
+  employees:Employee[];
+  constructor(private http: HttpClient) {
+    this.selectedEmployee=new Employee();
+    this.employees=[];
+  }
+  getEmployees() {
+    return this.http.get(this.employeesApi);
+  }
+  postEmployees(employee: Employee) {
+    return this.http.post(this.employeesApi, employee);
+  }
+  putEmployees(employee: Employee) {
+    return this.http.put(this.employeesApi + `/${employee._id}`, employee);
+  }
+  deleteEmployees(employee: Employee) {
+    return this.http.delete(this.employeesApi + `/${employee._id}`);
+  }
 }
