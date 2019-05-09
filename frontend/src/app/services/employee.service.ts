9e10602a5a66f0be23bac2d0918e4634acdae4c0
mgaticah <manuel.gatica@datco.cl>
2019-05-09 15:26:28 -0400

    habilita EmployeeService para acceder hacer requests http y referir a la clase employee

--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
+import { Employee } from '../models/employee';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  selectedEmployee:Employee;
+  constructor(private http:HttpClient) {
+
+   }
 }
