792d82cfe2aba1f3143857cd69fec81fd40c8899
mgaticah <manuel.gatica@datco.cl>
2019-05-10 11:35:50 -0400

    método de búsqueda de empleados en componente employees

--- frontend/src/app/components/employees/employees.component.ts
+++ frontend/src/app/components/employees/employees.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
 import { NgForm } from '@angular/forms';
 import { Employee } from 'src/app/models/employee';
-declare var M:any;
+declare var M: any;
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
@@ -16,6 +16,7 @@ export class EmployeesComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getEmployees();
   }
   resetForm(form?: NgForm) {
     if (form) {
@@ -23,11 +24,17 @@ export class EmployeesComponent implements OnInit {
     }
     this.employeeService.selectedEmployee = new Employee();
   }
-  addEmployee(form:NgForm)
-  {
-    this.employeeService.postEmployees(form.value).subscribe(result=>{
+  addEmployee(form: NgForm) {
+    this.employeeService.postEmployees(form.value).subscribe(result => {
       this.resetForm(form);
-      M.toast({html:'Empleado grabado'});
+      M.toast({ html: 'Empleado grabado' });
     });
+  }
+  getEmployees() {
+    this.employeeService.getEmployees().subscribe(
+      res => {
+        this.employeeService.employees = res as Employee[];
+      }
+    );
   }
 }
